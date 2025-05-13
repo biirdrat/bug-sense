77e82bddfca216ed5f2652df2cd739604db89652
biirdrat <steventubusiness@gmail.com>
2025-05-12 20:48:01 -0500

    feat: add QTCharts to CMake

--- ui-interface/src/main_window.cpp
+++ ui-interface/src/main_window.cpp
@@ -1,12 +1,18 @@
 #include "main_window.h"
 #include "ui_main_window.h"
 
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+
+QT_CHARTS_USE_NAMESPACE
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
 }
 
 MainWindow::~MainWindow()
